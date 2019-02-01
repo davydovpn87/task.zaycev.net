6a51996f1495e5618a95a4d60745fd23bf4f0a0f
davydovpn <davydovpn@ya.ru>
2019-02-01 14:17:09 +0500

    123

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         // Создание списка входных параметров
-        ArrayList<Integer> inputVariables = new ArrayList<Integer>();
+        ArrayList<Integer> inputVariables = new ArrayList<>();
         Collections.addAll(inputVariables, 0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11);
-        // Переменные
-        StringBuilder sb; String result; int count = 0;
+        String result;
+        int count = 0;
         // Цикл с еднственной точкой выхода - бесконечный цикл
         while (true) {
             // Сохранение состояния массива в строку конкатенацией всех значений массива
-            sb = new StringBuilder();
-            for (Integer i:inputVariables) {sb.append(i);}
+            StringBuilder sb = new StringBuilder();
+            inputVariables.forEach(sb::append);
             result =  sb.toString();
             // Проверка на наличие в мапе существовавших состояний
             if (log.containsKey(result)) {
@@ -25,7 +25,6 @@ public class Main {
                 break;
             }else{
                 log.put(result, count);
-                //System.out.println(result + "    " +  count);//для отладки
                 count++;
             }
             // Распределение максимального элемента
@@ -34,17 +33,17 @@ public class Main {
     }
 
     // Выполняет бизнес логику (распределение максимального значения) входного массива
-    public static synchronized void respectValuesOfArray(ArrayList<Integer> list){
+    public static void respectValuesOfArray(List<Integer> list){
         int maxElementOfArray = Collections.max(list);
         int indexOfMaximumValue = list.indexOf(maxElementOfArray);
         int indexToUpdate = 0;
-        // Обнулим ячейку для перераспределения
+        // Обнуляю максимальное значение, чтобы не делать проверку в цикле на то попал ли я в ячейку с максимальным значением
         list.set(indexOfMaximumValue, 0);
         // Пробегаем с ячейки следующей за ячейкой с максимальным значением и последовательно инкрементим значения
         for (int i = 1; i <= maxElementOfArray; i++) {
             // Проверка на выход за пределы массива
-            indexToUpdate = indexOfMaximumValue + i >= list.size() ? (indexOfMaximumValue + i) % list.size() : indexOfMaximumValue + i;
-            list.set(indexToUpdate, list.get(indexToUpdate) + 1); // сам инкремент
+            indexToUpdate = (indexOfMaximumValue + i) % list.size();
+            list.set(indexToUpdate, list.get(indexToUpdate) + 1);
         }
     }
 }
